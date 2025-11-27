e820fbb6463986b74f9dff54c4f850d699bcb81f
Leandro <23118944+lribeiromoura@users.noreply.github.com>
2025-11-27 15:42:13 -0300

    feat: create searchbar

--- src/pages/MicromarketPage.tsx
+++ src/pages/MicromarketPage.tsx
@@ -7,21 +7,53 @@ import { micromarketsData } from "@/data/micromarkets";
 import { Button } from "@/components/ui/button";
 
 import { SidebarRoot } from "@/components/sidebar/root";
+import MicromarketSearchBar from "@/components/search-bar";
+import type { MicromarketFilters } from "@/types/Micromarkets";
 
 const MicromarketPage = () => {
   const [openDrawer, setOpenDrawer] = useState(false);
+
+  const [filteredMicromarkets, setFilteredMicromarkets] =
+    useState(micromarketsData);
 
   const [selectedId, setSelectedId] = useState<string | null>(
     micromarketsData[0]?.id ?? null
   );
 
+  const [filters, setFilters] = useState<MicromarketFilters>({
+    region: "all",
+    status: "all",
+  });
+
+  const handleFilter = () => {
+    let filtered = micromarketsData;
+
+    if (filters.region !== "all") {
+      filtered = filtered.filter((m) => m.region === filters.region);
+    }
+
+    if (filters.status !== "all") {
+      const isActive = filters.status === "active";
+      filtered = filtered.filter((m) => m.active === isActive);
+    }
+
+    setFilteredMicromarkets(filtered);
+
+    setSelectedId(filtered[0]?.id ?? null);
+  };
+
   return (
     <div className="w-full flex flex-col overflow-hidden h-full">
+      <MicromarketSearchBar
+        filters={filters}
+        onChangeFilters={setFilters}
+        onSubmit={handleFilter}
+      />
       <div className="flex flex-1 overflow-hidden">
         <SidebarRoot
           selectedId={selectedId}
           setSelectedId={setSelectedId}
-          filteredMicromarkets={micromarketsData}
+          filteredMicromarkets={filteredMicromarkets}
           openDrawer={openDrawer}
           setOpenDrawer={setOpenDrawer}
         />
